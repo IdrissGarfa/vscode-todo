f15a07d016c4098dd187f8f053ebb15b1c0a926a
Idriss Garfa <idrissgarfa@gmail.com>
2023-01-13 22:28:35 +0100

    webviews

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import { HelloWorldPanel } from './HelloWorldPanel';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -6,7 +7,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('vstodo.helloWorld', () => {
-			vscode.window.showInformationMessage('Hello from vsTodo!');
+			HelloWorldPanel.createOrShow(context.extensionUri);
 		})
 	);
 
